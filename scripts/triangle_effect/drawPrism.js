0abd3bbbd9974d361c8d5c5f1135f51f9d7795ed
asa55 <asa55@pitt.edu>
2019-06-28 13:21:41 -0400

    refactored js files (formPrism ~250 lines now 97)

--- scripts/triangle_effect/drawPrism.js
+++ scripts/triangle_effect/drawPrism.js
@@ -1,13 +1,13 @@
 function drawPrism() {
-    colorchoices = ['rgba(0,0,0,0.3)']
+    let colorchoices = ['rgba(0,0,0,0.3)']
 
     for (i=0; i<triangleData.length; i++) {
-        ithTriangleSet = triangleData[i]
-        var polyElement = document.createElementNS("http://www.w3.org/2000/svg", 'polygon');
-        polypoints = ithTriangleSet[0][0] + ',' +ithTriangleSet[0][1] + ' ' + ithTriangleSet[1][0] + ',' + ithTriangleSet[1][1] + ' ' + ithTriangleSet[2][0] + ',' + ithTriangleSet[2][1]
+        let ithTriangleSet = triangleData[i]
+        let polyElement = document.createElementNS("http://www.w3.org/2000/svg", 'polygon');
+        let polypoints = ithTriangleSet[0][0] + ',' +ithTriangleSet[0][1] + ' ' + ithTriangleSet[1][0] + ',' + ithTriangleSet[1][1] + ' ' + ithTriangleSet[2][0] + ',' + ithTriangleSet[2][1]
         polyElement.setAttribute('points',polypoints);
         polyElement.setAttribute('id','triangleNo' + i);
-        polyElement.style.fill = colorchoices[parseInt(Math.random()*(colorchoices.length))];
+        polyElement.style.fill = colorchoices[0];
         polyElement.style.strokeWidth = '0.05%';
         polyElement.style.stroke = 'rgba(255,255,255,0.3)';
         dynamicSvgTriangleSlide.appendChild(polyElement);
--- scripts/triangle_effect/giveColor.js
+++ scripts/triangle_effect/giveColor.js
@@ -1,9 +1,10 @@
 function giveColor(event) {
-    var bRect = dynamicSvgTriangleSlide.getBoundingClientRect();
-    posX = event.clientX - bRect.left;
-    posY = event.clientY - bRect.top;
-    colorchoice = colorPick(posX,posY)
+    let bRect = dynamicSvgTriangleSlide.getBoundingClientRect();
+    let posX = event.clientX - bRect.left;
+    let posY = event.clientY - bRect.top;
+    let colorchoice = colorPick(posX,posY)
+    
     event.target.style.fill = colorchoice
     event.target.style.stroke = colorchoice
-    colorchosenStatus[event.target.id.toString()] = [1,colorPick(posX,posY)]
+    colorchosenStatus[event.target.id.toString()] = [1,colorchoice]
 }
